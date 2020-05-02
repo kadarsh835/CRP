b5319afa89d36f3bd9d918a1756909355b850019
kadarsh835 <2017csb1064@iitrpr.ac.in>
2020-05-03 03:41:55 +0530

    Add Google login and test it

--- CRP/settings.py
+++ CRP/settings.py
@@ -36,13 +36,22 @@ INSTALLED_APPS = [
     'course',
     'faculty',
     'miscellaneous',
+
     #defaults
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #allauth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+
 ]
 
 MIDDLEWARE = [
@@ -124,3 +133,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Django All-auth settings
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of 'allauth
+    'django.contrib.auth.backends.ModelBackend',
+    # Allauth specific authentication method, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID=1
