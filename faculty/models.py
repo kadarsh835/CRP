ed0a43e9918ad44cf05c030e5181dbc63fefbd4b
Adarsh Kumar <adkumar@chegg.com>
2021-04-05 02:10:16 +0530

    Remove extra fields from the faculty model

--- faculty/models.py
+++ faculty/models.py
@@ -4,26 +4,26 @@ from django.contrib.auth.models import User
 from course.models import Course, CourseSection
 from miscellaneous.models import Department
 
-SEMESTER= (
+SEMESTER = (
     (1, 'First'),
     (2, 'Second'),
 )
 
 
 class Faculty(models.Model):
-    user= models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    department= models.ForeignKey(Department, on_delete= models.SET_NULL, null= True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    department = models.ForeignKey(
+        Department, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.user.__str__()
 
 
 class FacultyTakesCourse(models.Model):
-    faculty= models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True)
-    course= models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
-    section= models.ForeignKey(CourseSection, on_delete=models.SET_NULL, null=True)
-    semester= models.CharField(max_length=1, choices=SEMESTER)
-    year= models.SmallIntegerField()
+    faculty = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True)
+    section = models.ForeignKey(
+        CourseSection, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        self.course.code+' '+str(self.year)+' Sem: '+self.semester
+        return self.section.course.course_code+' ' + \
+            str(self.section.year)+' Sem: ' + self.section.semester
